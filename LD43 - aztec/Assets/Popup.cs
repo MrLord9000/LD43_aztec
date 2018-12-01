c813584d73d6d05f89a021b2eff811a50c069bff
Ignolek <ignole@icloud.com>
2018-12-01 18:57:31 +0100

    HOUSE IS WORKING YEA

---
+++ LD43 - aztec/Assets/Popup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup : MonoBehaviour {
+
+    public GameObject buttonBox;
+
+    private void OnValidate()
+    {
+        buttonBox.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        if(buttonBox.active)
+            buttonBox.SetActive(false);
+        else
+            buttonBox.SetActive(true);
+
+    }
+}
