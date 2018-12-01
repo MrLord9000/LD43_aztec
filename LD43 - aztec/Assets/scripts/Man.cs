d13a7deaf9464bba644dadf6fed9508248e6cc60
Andrzej M <andrzej.mi@op.pl>
2018-12-01 18:08:13 +0100

    Added Peoples dynamic sprite script

--- LD43 - aztec/Assets/scripts/Man.cs
+++ LD43 - aztec/Assets/scripts/Man.cs
@@ -3,32 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
+public enum Age
+{
+    child,
+    adult,
+    old
+}
+
+public enum Sex
+{
+    female,
+    male
+}
+public enum Role
+{
+    worker,
+
+    builder,
+    farmer,
+    soldier
+}
+public enum Variant
+{
+    none,
+    _1,
+    _2,
+    _3
+}
+
 public class Man : MonoBehaviour
 {
-
     public int expRequire;
 
-    public enum Age
-    {
-        child,
-        adult,
-        old
-    }
-    public enum Role
-    {
-        worker,
-
-        builder,
-        farmer,
-        soldier
-    }
-    public enum Variant
-    {
-        none,
-        _1,
-        _2,
-        _3
-    }
     [System.Serializable]
     public class Level
     {
@@ -38,6 +45,8 @@ public class Man : MonoBehaviour
     }
 
     public string _name;
+
+    public Sex sex;
     public Role role;
     public Variant variant = Variant.none;
 
@@ -111,147 +120,3 @@ public class Man : MonoBehaviour
         }
     }
 }
-
-
-
-
-/*
-	// Use this for initialization
-	void Start ()
-    {
-        old = (int)Random.Range(12, 16);
-        old = (int)Random.Range(17, 22);
-
-        StartCoroutine(Work(Role.builder));
-        StartCoroutine(Work(Role.farmer));
-        StartCoroutine(Work(Role.soldier));
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        expirience();
-
-        
-    }
-
-    private IEnumerator Work(Role role)
-    {
-        while (true)
-        {
-            switch (role)
-            {
-                case Role.builder:
-                    if (work_as_builder)
-                    {
-                        if (builderExp >= expRequire)
-                        {
-                            builderExp = 0;
-                            builderLevel++;
-                        }
-                        else
-                        {
-                            builderExp++;
-                        }
-                    }
-                    break;
-
-                case Role.farmer:
-                    if (work_as_farmer)
-                    {
-                        if (farmerExp >= expRequire)
-                        {
-                            farmerExp = 0;
-                            farmerLevel++;
-                        }
-                        else
-                        {
-                            farmerExp++;
-                        }
-                    }
-                    break;
-
-                case Role.soldier:
-                    if (work_as_soldier)
-                    {
-                        if (soldierExp >= expRequire)
-                        {
-                            soldierExp = 0;
-                            soloderLevel++;
-                        }
-                        else
-                        {
-                            soldierExp++;
-                        }
-                    }
-                    break;
-            }
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
-
-    private void expirience()
-    {
-        if (isMaxed) return;
-
-        switch (role)
-        {
-            case Role.builder:
-                if (builderLevel >= 10)
-                {
-                    isMaxed = true;
-                    StopCoroutine(Work(Role.builder));
-                    builderExp = 0;
-                }
-                StopCoroutine(Work(Role.farmer));
-                StopCoroutine(Work(Role.soldier));
-                work_as_farmer = false;
-                work_as_soldier = false;
-                return;
-
-            case Role.farmer:
-                if (farmerLevel >= 10)
-                {
-                    isMaxed = true;
-                    StopCoroutine(Work(Role.farmer));
-                    farmerExp = 0;
-                }
-                StopCoroutine(Work(Role.builder));
-                StopCoroutine(Work(Role.soldier));
-                work_as_builder = false;
-                work_as_soldier = false;
-                return;
-
-            case Role.soldier:
-                if (soloderLevel >= 10)
-                {
-                    isMaxed = true;
-                    StopCoroutine(Work(Role.soldier));
-                    soldierExp = 0;
-                }
-                StopCoroutine(Work(Role.farmer));
-                StopCoroutine(Work(Role.builder));
-                work_as_builder = false;
-                work_as_farmer = false;
-                return;
-
-            case Role.worker:
-                if (builderLevel >= 4)
-                {
-                    builderLevel = 4;
-                    builderExp = 0;
-                }
-                if (farmerLevel >= 4)
-                {
-                    farmerLevel = 4;
-                    farmerExp = 0;
-                }
-                if (soloderLevel >= 4)
-                {
-                    soloderLevel = 4;
-                    soldierExp = 0;
-                }
-                break;
-        }
-    }
-}
-*/
---
+++ LD43 - aztec/Assets/scripts/ManSprite.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManSprite : MonoBehaviour {
+
+    [System.Serializable]
+    public class SpritesArchve
+    {
+        // WORKER
+        public Sprite WorkerF;
+        public Sprite WorkerM;
+
+         // 1 TIER BUILDER
+        public Sprite BuilderF;
+        public Sprite BuilderM;
+
+         // 2 TIER BUILDER
+        public Sprite Builder1F;
+        public Sprite Builder1M;
+        public Sprite Builder2F;
+        public Sprite Builder2M;
+        public Sprite Builder3F;
+        public Sprite Builder3M;
+
+         // 1 TIER FARMER
+        public Sprite FarmerF;
+        public Sprite FarmerM;
+         
+         // 2 TIER FARMER
+        public Sprite Farmer1F;
+        public Sprite Farmer1M;
+        public Sprite Farmer2F;
+        public Sprite Farmer2M;
+        public Sprite Farmer3F;
+        public Sprite Farmer3M;
+         
+         // 1 TIER SOLDIER
+        public Sprite SoldierF;
+        public Sprite SoldierM;
+         
+         // 2 TIER SOLDIER
+        public Sprite Soldier1F;
+        public Sprite Soldier1M;
+        public Sprite Soldier2F;
+        public Sprite Soldier2M;
+        public Sprite Soldier3F;
+        public Sprite Soldier3M;
+    }
+
+    public SpritesArchve spriteArchive;
+    private SpriteRenderer spriteRenderer;
+    private Man man;
+
+    // Use this for initialization
+    void Start () {
+        man = GetComponent<Man>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if( man.sex == Sex.male )
+            spriteRenderer.sprite = spriteArchive.WorkerM;
+        else
+            spriteRenderer.sprite = spriteArchive.WorkerF;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch( man.role )
+        {
+            case Role.worker:
+                if (man.sex == Sex.male)
+                    spriteRenderer.sprite = spriteArchive.WorkerM;
+                else
+                    spriteRenderer.sprite = spriteArchive.WorkerF;
+                return;
+
+            case Role.builder:
+                switch (man.variant)
+                {
+                    case Variant.none:
+                        if (man.sex == Sex.male)
+                            spriteRenderer.sprite = spriteArchive.BuilderM;
+                        else                                 
+                            spriteRenderer.sprite = spriteArchive.BuilderF;
+                        return;                               
+                                                              
+                    case Variant._1:                          
+                        if (man.sex == Sex.male)              
+                            spriteRenderer.sprite = spriteArchive.Builder1M;
+                        else                                   
+                            spriteRenderer.sprite = spriteArchive.Builder1F;
+                        return;                               
+                                                              
+                    case Variant._2:                          
+                        if (man.sex == Sex.male)              
+                            spriteRenderer.sprite = spriteArchive.Builder2M;
+                        else                                      
+                            spriteRenderer.sprite = spriteArchive.Builder2F;
+                        return;                               
+                                                              
+                    case Variant._3:                          
+                        if (man.sex == Sex.male)              
+                            spriteRenderer.sprite = spriteArchive.Builder3M;
+                        else                                      
+                            spriteRenderer.sprite = spriteArchive.Builder3F;
+                        return;
+                }
+                break;
+
+            case Role.farmer:
+                switch (man.variant)
+                {
+                    case Variant.none:
+                        if (man.sex == Sex.male)
+                            spriteRenderer.sprite = spriteArchive.FarmerM;
+                        else
+                            spriteRenderer.sprite = spriteArchive.FarmerF;
+                        return;
+
+                    case Variant._1:
+                        if (man.sex == Sex.male)
+                            spriteRenderer.sprite = spriteArchive.Farmer1M;
+                        else
+                            spriteRenderer.sprite = spriteArchive.Farmer1F;
+                        return;
+
+                    case Variant._2:
+                        if (man.sex == Sex.male)
+                            spriteRenderer.sprite = spriteArchive.Farmer2M;
+                        else
+                            spriteRenderer.sprite = spriteArchive.Farmer2F;
+                        return;
+
+                    case Variant._3:
+                        if (man.sex == Sex.male)
+                            spriteRenderer.sprite = spriteArchive.Farmer3M;
+                        else
+                            spriteRenderer.sprite = spriteArchive.Farmer3F;
+                        return;
+                }
+                break;
+            case Role.soldier:
+                switch (man.variant)
+                {
+                    case Variant.none:
+                        if (man.sex == Sex.male)
+                            spriteRenderer.sprite = spriteArchive.SoldierM;
+                        else                               
+                            spriteRenderer.sprite = spriteArchive.SoldierF;
+                        return;                              
+                                                             
+                    case Variant._1:                         
+                        if (man.sex == Sex.male)             
+                            spriteRenderer.sprite = spriteArchive.Soldier1M;
+                        else                             
+                            spriteRenderer.sprite = spriteArchive.Soldier1F;
+                        return;                                
+                                                               
+                    case Variant._2:                           
+                        if (man.sex == Sex.male)               
+                            spriteRenderer.sprite = spriteArchive.Soldier2M;
+                        else                                
+                            spriteRenderer.sprite = spriteArchive.Soldier2F;
+                        return;                               
+                                                              
+                    case Variant._3:                          
+                        if (man.sex == Sex.male)              
+                            spriteRenderer.sprite = spriteArchive.Soldier3M;
+                        else                                
+                            spriteRenderer.sprite = spriteArchive.Soldier3F;
+                        return;
+                }
+                break;
+        }		
+	}
+}
