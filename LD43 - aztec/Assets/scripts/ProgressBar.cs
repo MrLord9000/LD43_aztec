0b2277a3251ec68eea70ad416483d17993f4165c
Pawełek <pawelkun@gmail.com>
2018-12-01 18:55:58 +0100

    Added ProgressBar.cs to scripts

---
+++ LD43 - aztec/Assets/scripts/ProgressBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour {
+
+    Button progressBar;
+    public float maxTime = 10f;
+    private float timeLeft;
+    
+	
+    // Use this for initialization
+	void Start ()
+    {
+        progressBar = GetComponent<Button>();
+        timeLeft = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            progressBar.image.fillAmount = 1 - timeLeft / maxTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+}
