fe0790b0e304c5cc6f29c6ce9f507cc7397255f6
unknown <szym.wrobel@gmail.com>
2018-12-02 13:55:24 +0100

    Added some sprites

--- LD43 - aztec/Assets/Credits.cs
+++ LD43 - aztec/Assets/Credits.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Options : MonoBehaviour {
+public class Credits : MonoBehaviour
+{
 
     public void BackButton()
     {
--- LD43 - aztec/Assets/MainMenu.cs
+++ LD43 - aztec/Assets/MainMenu.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class MainMenu : MonoBehaviour {
 
     public void PlayGame ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Game");
     }
     public void Options ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene("Options");
     }
-
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void BackButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
