49ccc00c0aff6122c3957cad666bde5e48c7aa39
unknown <szym.wrobel@gmail.com>
2018-12-04 01:09:46 +0100

    Replaced few assets

---
+++ LD43 - aztec/Assets/scripts/PlayAndExit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Linq;
+using UnityEngine.UI;
+
+public class PlayAndExit : MonoBehaviour {
+
+    private bool show = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        this.gameObject.SetActive(show);
+
+    }
+    private void OnMouseOver()
+    {
+        Debug.Log("a");
+        show = true;
+    }
+    private void OnMouseExit()
+    { 
+        show = false;
+    }
+
+}
