3357bb2f91b27ec842a6aee52a93629c54320d44
MrLord9000 <filip1209348756@gmail.com>
2019-02-27 18:35:30 +0100

    Fixed tile alignment, added basic tile selection visuals

--- LD43 - aztec/Assets/scripts/TileSelect.cs
+++ LD43 - aztec/Assets/scripts/TileSelect.cs
@@ -8,12 +8,21 @@ public class TileSelect : MonoBehaviour
     private GameObject tileSpritePrefab;
     private GameObject tileSprite;
 
+    [SerializeField]
+    private Color hoverColor;
+
+    [SerializeField]
+    private Color activeColor;
+
+    private bool isClicked = false;
+
     public Vector2 currentTileCoordinates = new Vector2(0,0);
 
     // Start is called before the first frame update
     void Start()
     {
         tileSprite = Instantiate(tileSpritePrefab, new Vector2(0, 0), Quaternion.identity);
+        tileSprite.GetComponent<SpriteRenderer>().color = hoverColor;
     }
 
     // Update is called once per frame
@@ -21,5 +30,22 @@ public class TileSelect : MonoBehaviour
     {
         currentTileCoordinates = TileSystem.GridBaseToCartesian(TileSystem.CartesianToTile(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
         tileSprite.transform.position = currentTileCoordinates;
+        if(Input.GetKeyDown(KeyCode.Mouse0) && !isClicked)
+        {
+            isClicked = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse0) && isClicked)
+        {
+            isClicked = false;
+        }
+
+        if(isClicked)
+        {
+            tileSprite.GetComponent<SpriteRenderer>().color = activeColor;
+        }
+        else
+        {
+            tileSprite.GetComponent<SpriteRenderer>().color = hoverColor;
+        }
     }
 }
