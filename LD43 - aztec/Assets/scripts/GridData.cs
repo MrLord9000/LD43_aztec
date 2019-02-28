5018a20bef00b7b771bd2ca1a77a3ef4dae0c66b
MrLord9000 <filip1209348756@gmail.com>
2019-02-28 01:14:59 +0100

    Started working on proper grid mechanics

---
+++ LD43 - aztec/Assets/scripts/GridData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Pair
+{
+    public Vector2 position;
+    public GameObject gameObject;
+
+    public Pair(Vector2 pos, GameObject gameObj)
+    {
+        position = pos;
+        gameObject = gameObj;
+    }
+}
+
+[System.Serializable]
+public class GridContainer
+{
+    [SerializeField]
+    private List<Pair> pairs;
+
+    public GameObject this[Vector2 position]
+    {
+        get
+        {
+            foreach (Pair pair in pairs)
+            {
+                if (pair.position == position)
+                {
+                    return pair.gameObject;
+                }
+            }
+            throw new IndexOutOfRangeException();
+        }
+
+        set
+        {
+            for (int i = 0; i < pairs.Count; i++)
+            {
+                if (pairs[i].position == position)
+                {
+                    pairs[i].gameObject = value;
+                    return;
+                }
+            }
+            pairs.Add(new Pair(position, value));
+
+        }
+    }
+}
+
+[CreateAssetMenu(fileName = "GridData", menuName = "ScriptableObjects/GridData", order = 1)]
+public class GridData : ScriptableObject
+{
+    public GridContainer gridBuildings;
+}
---
+++ LD43 - aztec/Assets/scripts/GridInteraction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridInteraction : MonoBehaviour
+{
+    public GridData gridData;
+
+    public void AssignToTile(Vector2 tilePos, GameObject building)
+    {
+        //gridData.gridBuildings;
+    }
+}
