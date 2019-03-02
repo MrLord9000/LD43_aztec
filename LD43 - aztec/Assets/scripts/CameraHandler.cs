3fd77e8e52e4b639f3cf6fbf49a8257c4c4478e6
MrLord9000 <filip1209348756@gmail.com>
2019-03-03 00:12:18 +0100

    Continuing work on grid drawing - spawining grid now works independently of screen resolution

--- LD43 - aztec/Assets/scripts/CameraHandler.cs
+++ LD43 - aztec/Assets/scripts/CameraHandler.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseDrag : MonoBehaviour {
+public class CameraHandler : MonoBehaviour {
 
     public float baseSpeed = 100f;
     private float speed;
-    [Tooltip("Should be a half of max screen height in pixels.")]
-    public int maxSize = 540;
+    // Max size is determined in Awake(), and it tells us the half of screen height.
+    // This is especially useful in grid spawining so keep it in mind
+    private static float maxSize;
+    private Camera mainCamera;
+
+    public static float GetMaxCameraSize()
+    {
+        return maxSize;
+    }
 
     private void Awake()
     {
+        mainCamera = gameObject.GetComponent<Camera>();
         speed = baseSpeed;
+        maxSize = Screen.height / 2f;
+        Debug.Log("MaxSize: " + maxSize);
+        mainCamera.orthographicSize = maxSize;
     }
 
     private void Update()
@@ -21,17 +32,16 @@ public class MouseDrag : MonoBehaviour {
             //if(transform.position.x < boundsX && transform.position.x > -boundsX && transform.position.y < boundsY && transform.position.y > -boundsY)
             transform.position -= new Vector3(  Input.GetAxis("Mouse X") * speed * Time.deltaTime,
                                                 Input.GetAxis("Mouse Y") * speed * Time.deltaTime);
-
         }
         if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
             speed = baseSpeed;
-            Camera.main.orthographicSize = 270;
+            mainCamera.orthographicSize = maxSize / 2f;
         }
         else if(Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
             speed = baseSpeed * 2f;
-            Camera.main.orthographicSize = 2 * 270;
+            mainCamera.orthographicSize = maxSize;
         }
 
     }
--- LD43 - aztec/Assets/scripts/GridDrawing.cs
+++ LD43 - aztec/Assets/scripts/GridDrawing.cs
@@ -13,7 +13,8 @@ public class GridDrawing : MonoBehaviour
 
     public GameObject gridSprite;
     public GameObject spriteMask;
-    public GameObject tileContainer;
+    public Transform tileContainer;
+    //private GameObject[] allTiles;
 
     private Camera mainCamera;
     private Vector2 cameraDim;
@@ -21,62 +22,106 @@ public class GridDrawing : MonoBehaviour
     public void Awake()
     {
         mainCamera = Camera.main;
+        cameraDim.Set(CameraHandler.GetMaxCameraSize() * mainCamera.aspect, CameraHandler.GetMaxCameraSize());
     }
 
     public void Start()
     {
-        SpawnExtended(new Vector2(0, 0));
-        TileLoopScreen(testTile);
+        SpawnExtended();
+        //TileLoopScreen(testTile.transform);
     }
 
     public void Update()
     {
         //Debug.Log("Camera dimensions: x = " + mainCamera.orthographicSize * mainCamera.aspect + " y = " + mainCamera.orthographicSize);
-        cameraDim.Set(mainCamera.orthographicSize * mainCamera.aspect, mainCamera.orthographicSize);
-        TileLoopScreen(testTile);
+        //cameraDim.Set(mainCamera.orthographicSize * mainCamera.aspect, mainCamera.orthographicSize);
+
+        //allTiles = tileContainer.GetComponentsInChildren<GameObject>();
+        for (int i = 0; i < tileContainer.childCount; i++)
+        {
+            TileLoopScreen(tileContainer.GetChild(i));
+        }
+        //TileLoopScreen(testTile.transform);
 
         Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         spriteMask.transform.position = mousePos;
     }
 
-    private void TileLoopScreen(GameObject tile)
+    private void TileLoopScreen(Transform tile)
     {
-        Vector2 tileCenter = tile.gameObject.transform.position;
+        Vector2 tilePivot = tile.position;
         Vector2 tileDim = tile.GetComponent<SpriteRenderer>().sprite.rect.size;
-        //Debug.Log("Tile center: " + tileCenter);
-        //Debug.Log("Tile dimensions: " + tileDim);
-        //Debug.Log("Camera center: " + mainCamera.transform.position);
-        Debug.Log("Camera dimensions: " + cameraDim);
-        // Checking the left to right screen bounds
-        if (tileCenter.x + tileDim.x/2f < mainCamera.transform.position.x - (cameraDim.x + bufferOffset))
-        {
-            //Debug.Log("Should move the tile now.");
-            //tile.transform.position.Set(tile.transform.position.x + tileDim.x * Mathf.Floor( (cameraDim.x * 2f)/ tileDim.x), tile.transform.position.y, 0);
-            tile.transform.position = new Vector2(tile.transform.position.x + tileDim.x * Mathf.Floor((cameraDim.x * 2f) / tileDim.x), tile.transform.position.y);
-        }
 
+        // Checking the left to right screen bounds
+        if (tilePivot.x + tileDim.x < mainCamera.transform.position.x - (cameraDim.x + bufferOffset))
+        {
+            tile.transform.position =   TileSystem.GridBaseToCartesian(
+                                        TileSystem.CartesianToTile(new Vector2(tile.transform.position.x + tileDim.x * Mathf.Ceil( ((cameraDim.x * 2f) + bufferOffset) / tileDim.x ), tile.transform.position.y)));
+        }
+        else if (tilePivot.x > mainCamera.transform.position.x + (cameraDim.x + bufferOffset))
+        {
+            tile.transform.position =   TileSystem.GridBaseToCartesian(
+                                        TileSystem.CartesianToTile(new Vector2(tile.transform.position.x - tileDim.x * Mathf.Ceil( ((cameraDim.x * 2f) + bufferOffset) / tileDim.x ), tile.transform.position.y)));
+        }
+        if (tilePivot.y + tileDim.y < mainCamera.transform.position.y - (cameraDim.y + bufferOffset))
+        {
+            tile.transform.position =   TileSystem.GridBaseToCartesian(
+                                        TileSystem.CartesianToTile(new Vector2(tile.transform.position.x, tile.transform.position.y + tileDim.y * Mathf.Ceil(((cameraDim.y * 2f) + bufferOffset) / tileDim.y))));
+        }
+        else if (tilePivot.y - tileDim.y > mainCamera.transform.position.y + (cameraDim.y + bufferOffset))
+        {
+            tile.transform.position =   TileSystem.GridBaseToCartesian(
+                                        TileSystem.CartesianToTile(new Vector2(tile.transform.position.x, tile.transform.position.y - tileDim.y * Mathf.Ceil(((cameraDim.y * 2f) + bufferOffset) / tileDim.y))));
+        }
     }
 
-    private void SpawnExtended(Vector2 origin)
+    private void SpawnExtended()
     {
-        origin = new Vector2(origin.x - gridExtent/2, origin.y - gridExtent/2);
-        for(int i = 0; i < gridExtent; i++)
+        Vector2 leftTopBounds = TileSystem.CartesianToTile(new Vector2(mainCamera.transform.position.x - (cameraDim.x + bufferOffset), mainCamera.transform.position.y + (cameraDim.y + bufferOffset)));
+        //float leftTileBounds = leftTopBounds.x;
+        //Debug.Log("Left top bounds: " + leftTopBounds);
+        //Debug.Log("X: " + (mainCamera.transform.position.x - (cameraDim.x + bufferOffset)) );
+        //Debug.Log("Y: " + (mainCamera.transform.position.y + (cameraDim.y + bufferOffset)) );
+        //Vector2 rightTopBounds = TileSystem.CartesianToTile(new Vector2(mainCamera.transform.position.x + (cameraDim.x + bufferOffset), mainCamera.transform.position.y + (cameraDim.y + bufferOffset)));
+        //Vector2 leftBottomBounds = TileSystem.CartesianToTile(new Vector2(mainCamera.transform.position.x - (cameraDim.x + bufferOffset), mainCamera.transform.position.y - (cameraDim.y + bufferOffset)));
+        //Vector2 rightBottomBounds = TileSystem.CartesianToTile(new Vector2(mainCamera.transform.position.x + (cameraDim.x + bufferOffset), mainCamera.transform.position.y - (cameraDim.y + bufferOffset)));
+
+        Vector2 tileDim = gridSprite.GetComponent<SpriteRenderer>().sprite.rect.size;
+
+        //for (int i = 0; i < Mathf.Ceil( (cameraDim.y * 2f)/tileDim.y ); i++)
+        //{
+        //    for(int j = 0; j < Mathf.Ceil( (cameraDim.x * 2f)/tileDim.x ); j++)
+        //    {
+        //        //Vector2 cartesianOrigin = TileSystem.GridBaseToCartesian(TileSystem.CartesianToTile(new Vector2(leftTopBounds.x * (j + 1), leftTopBounds.y * (i + 1))));
+        //        //GameObject.Instantiate(gridSprite, cartesianOrigin, new Quaternion(0, 0, 0, 0), tileContainer);
+        //        gridCellSpawn(leftTopBounds.x + j, leftTopBounds.y - i);
+        //    }
+        //}
+
+        Vector2 origin = new Vector2( -(gridExtent / 2), -(gridExtent / 2) );
+        for (int i = 0; i < gridExtent; i++)
         {
-            for(int j = 0; j < gridExtent; j++)
+            for (int j = 0; j < gridExtent; j++)
             {
-                int i_temp = i;
-                //if (j % 2 == 1) i_temp++;
-                //if (i_temp % 2 == 0)
-                //{
-                    gridCellSpawn(origin.x + i, origin.y + j);
-                //}
+                gridCellSpawn(origin.x + i, origin.y + j);
+            }
+        }
+
+        Transform[] childTransforms = tileContainer.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < childTransforms.Length; i++)
+        {
+            if ( ((childTransforms[i].position.x < mainCamera.transform.position.x - (cameraDim.x + bufferOffset) || childTransforms[i].position.x > mainCamera.transform.position.x + (cameraDim.x + bufferOffset)) ||
+                  (childTransforms[i].position.y < mainCamera.transform.position.y - (cameraDim.y + bufferOffset) || childTransforms[i].position.y > mainCamera.transform.position.y + (cameraDim.y + bufferOffset))))
+            {
+                Destroy(childTransforms[i].gameObject);
             }
         }
     }
 
     private void gridCellSpawn(float x, float y)
     {
+        //Debug.Log("Spawning a tile!");
         Vector2 cartesianOrigin = TileSystem.GridBaseToCartesian(new Vector2(x, y));
-        GameObject.Instantiate(gridSprite, cartesianOrigin, new Quaternion(0, 0, 0, 0));
+        GameObject.Instantiate(gridSprite, cartesianOrigin, new Quaternion(0, 0, 0, 0), tileContainer);
     }
 }
